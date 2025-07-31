9192f278b6da390fe7682b404a4a4b6bd6a0e4ae
timlim24 <157886626+timlim24@users.noreply.github.com>
2025-08-01 00:17:52 +0300

    Add BallsRhythm

---
+++ Assets/Scripts/BallsRhythm.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using static Unity.VisualScripting.Metadata;
+
+public class BallsRhythm : MonoBehaviour
+{
+    public Transform[] children;
+    public int beat;
+    void Start()
+    {
+        beat = 0;
+        children = new Transform[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            children[i] = transform.GetChild(i);
+        }
+
+        RhythmManager.OnBeat += () =>
+        {
+            StartCoroutine(ScaleToTarget(new Vector3(.5f, .5f, .5f), 0.1f, beat));
+
+            beat++;
+            if (beat == 4)
+            {
+                beat = 0;
+            }
+            children[beat].localScale = new Vector3(1f, 1f, 1f);
+        };
+
+        IEnumerator ScaleToTarget(Vector3 target, float time, int beat)
+        {
+            Vector3 initialScale = children[beat].localScale;
+            float elapsed = 0f;
+
+            while (elapsed < time)
+            {
+                children[beat].localScale = Vector3.Lerp(initialScale, target, elapsed / time);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            //children[beat].localScale = target; 
+        }
+    }
+}
